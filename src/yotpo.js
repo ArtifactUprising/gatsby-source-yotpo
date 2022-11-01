fa8ae9ff48e98c854ab1708bc7f54f4b178308e7
Justin Pohlmann <justin.pohlmann@artifactuprising.com>
2022-11-01 10:39:42 -0600

    Updated package to use cached data.

--- src/yotpo.js
+++ src/yotpo.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import request from "request";
+const fs = require('fs');
 
 export const getAccessToken = ({ appKey, appSecret, page, pageSize }) => {
   const options = {
@@ -21,26 +23,43 @@ export const getAccessToken = ({ appKey, appSecret, page, pageSize }) => {
 };
 
 export const allReviews = ({ appKey, accessToken, page, pageSize }) => {
-  const options = {
-    method: "GET",
-    url: `https://api.yotpo.com/v1/apps/${appKey}/reviews`,
-    qs: {
-      utoken: accessToken,
-      page: page,
-      count: pageSize,
-    },
-    json: true,
-  };
-
   return new Promise((resolve, reject) => {
-    request(options, function (error, response, body) {
-      if (error) reject(error);
-
-      resolve(body.reviews);
+    console.log(`getting page ${page}`);
+    axios.get(`https://api.yotpo.com/v1/apps/${appKey}/reviews`, {
+      params: {
+        utoken: accessToken,
+        page: page,
+        count: pageSize,
+      },
+      timeout: 1500,
+      headers: {
+        accept: 'application/json'
+      }
+    }).then((response) => {
+      console.log('responded!');
+      resolve(response.data.reviews);
+    }).catch((error) => {
+      console.log('errored!');
+      reject(error);
     });
   });
 };
 
+export const getCachedData = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://assets.artifactuprising.com/assets/Yotpo/reviews.json`, {
+      timeout: 1500,
+      headers: {
+        accept: 'application/json'
+      }
+    }).then((response) => {
+      resolve(response.data);
+    }).catch((error) => {
+      console.log('errored!');
+      reject(error);
+    });
+  });
+}
 export const allProductBottomlines = ({ appKey, page, pageSize }) => {
   const options = {
     method: "GET",
